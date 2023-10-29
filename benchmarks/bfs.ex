f341a891d23e65037f5a3a140890ebfd5421993d
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-10-29 20:03:43 -0300

    bool to int

--- benchmarks/bfs.ex
+++ benchmarks/bfs.ex
@@ -3,13 +3,13 @@ defmodule BFS_Kernel do
 
   gpotion kernel1(starting, no_of_edges, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, max_threads_per_block) do
     tid = blockIdx.x * max_threads_per_block + threadIdx.x
-    if tid < no_of_nodes && graph_mask[tid] do
-      graph_mask = false
+    if tid < no_of_nodes and graph_mask[tid] == 1 do
+      graph_mask[tid] = 0
       for i in range(starting[tid], no_of_edges[tid] + starting[tid]) do
         id = graph_edges[i]
-        if !graph_visited[id] do
+        if graph_visited[id] == 1 do
           cost[id] = cost[tid] + 1
-          updating_graph_mask[id] = true
+          updating_graph_mask[id] = 1
         end
       end
     end
@@ -17,11 +17,11 @@ defmodule BFS_Kernel do
 
   gpotion kernel2(graph_mask, updating_graph_mask, graph_visited, over, no_of_nodes, max_threads_per_block) do
     tid = blockIdx.x * max_threads_per_block + threadIdx.x
-    if tid < no_of_nodes && updating_graph_mask[tid] do
-      graph_mask[tid] = true
-      graph_visited[tid] = true
-      over = true
-      updating_graph_mask[tid] = false
+    if tid < no_of_nodes && updating_graph_mask[tid] == 1 do
+      graph_mask[tid] = 1
+      graph_visited[tid] = 1
+      over[0] = 1
+      updating_graph_mask[tid] = 0
     end
   end
 end
@@ -34,10 +34,10 @@ max_threads_per_block = 512
 #   int starting
 #   int no_of_edges
 
-{:ok, content} = File.read("graph.txt")
-input = content
-  |> String.split
-  |> Enum.map(&String.to_integer/1)
+input = case File.read("graph.txt") do
+  {:ok, content} -> content |> String.split |> Enum.map(&String.to_integer/1)
+  {:error, reason} -> exit(reason)
+end
 
 no_of_nodes = Enum.at(input, 0)
 
@@ -53,14 +53,14 @@ nodes = Enum.slice(input, 1, no_of_nodes) |> Enum.chunk_every(2)
 
 starting = for i <- nodes do Enum.at(i, 0) end
 no_of_edges = for i <- nodes do Enum.at(i, 1) end
-graph_mask = List.duplicate(false, no_of_nodes)
-updating_graph_mask = List.duplicate(false, no_of_nodes)
-graph_visited = List.duplicate(false, no_of_nodes)
+graph_mask = List.duplicate(0, no_of_nodes)
+updating_graph_mask = List.duplicate(0, no_of_nodes)
+graph_visited = List.duplicate(0, no_of_nodes)
 
 source = 0
 
-graph_mask = List.replace_at(graph_mask, source, true)
-graph_visited = List.replace_at(graph_visited, source, true)
+graph_mask = List.replace_at(graph_mask, source, 1)
+graph_visited = List.replace_at(graph_visited, source, 1)
 
 edge_list_size = Enum.at(input, no_of_edges + 1)
 edges = Enum.slice(input, no_of_edges + 2, edge_list_size) |> Enum.chunk_every(2)
@@ -89,17 +89,15 @@ d_cost = List.duplicate(-1, no_of_nodes) |> List.replace_at(0, 0)
 m_d_cost = Matrex.new(d_cost)
 g_d_cost = GPotion.new_gmatrex(m_d_cost)
 
-over = [false]
+over = [0]
 m_over = Matrex.new(over)
 g_over = GPotion.new_gmatrex(m_over)
 
 copy_end = System.monotonic_time()
 
 
-IO.puts "Copied to GPU"
-
 k = 0
-stop = false
+stop = 0
 
 grid = {num_of_blocks, 1, 1}
 threads = {num_of_threads_per_block, 1, 1}
@@ -107,14 +105,20 @@ threads = {num_of_threads_per_block, 1, 1}
 kernel1 = GPotion.load(&BFS_Kernel.kernel1/10)
 kernel2 = GPotion.load(&BFS_Kernel.kernel2/7)
 
-Stream.unfold(false, fn
-  true ->
+exec = Stream.unfold(true, fn
+  false ->
     IO.puts "Iterations: #{k}"
     nil
-  false ->
+  true ->
+    g_over = GPotion.new_gmatrex(m_over)
     GPotion.spawn(kernel1, grid, threads, [g_starting, g_no_of_edges, g_graph_edges, g_graph_mask, g_updating_graph_mask, g_graph_visited, g_d_cost, no_of_nodes, max_threads_per_block])
     GPotion.spawn(kernel2, grid, threads, [g_graph_mask, g_updating_graph_mask, g_graph_visited, g_over, no_of_nodes, max_threads_per_block])
     r = GPotion.get_gmatrex(g_over)
     k = k + 1
-    {false, r[0]}
+    b = if r == 0, do: false, else: true
+    {true, b}
 end)
+
+Stream.run(exec)
+
+IO.puts "Copied to GPU: #{System.convert_time_unit(copy_end - copy_start, :native, :millisecond)}"
