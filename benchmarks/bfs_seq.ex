1227b694d9b05b857ec93298dd01c61916f1b781
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-12-08 13:33:21 -0300

    kernel 2

--- benchmarks/bfs_seq.ex
+++ benchmarks/bfs_seq.ex
@@ -1,20 +1,33 @@
 defmodule BFS_seq do
-  require IEx
-
   def function1(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes) do
-    mask_true = Enum.with_index(graph_mask)
+    mask_true = Stream.with_index(graph_mask)
     |> Stream.filter(fn {x, _} -> x == true end)
 
-    n = Enum.with_index(node_list )
+    n = Stream.with_index(node_list)
     |> Enum.filter(fn {{_, _}, index} -> Enum.member?(mask_true, {true, index}) end)
     |> Enum.map(fn {{s, e}, i} -> {s,e} end)
 
     {cost, updating_graph_mask} = for_each_node(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes)
   end
 
-  def function2(graph_mask, updating_graph_mask, graph_visited, over, no_of_nodes) do
-    l = for x = {true, _} <- updating_graph_mask |> Stream.with_index, do: x
-    {graph_mask, graph_visited, over, updating_graph_mask}
+  defp function2(graph_mask, updating_graph_mask, graph_visited, over, no_of_nodes) do
+    update_true = Stream.with_index(updating_graph_mask)
+    |> Stream.filter(fn {x, _} -> x == true end)
+    |> Enum.map(fn {_, i} -> i end)
+
+    verify_graph(graph_mask, updating_graph_mask, graph_visited, over, update_true)
+  end
+
+  defp verify_graph(graph_mask, updating_graph_mask, graph_visited, over, [index, others]) do
+    graph_mask = Enum.replace_at(graph_mask, index, true)
+    graph_visited = Enum.replace_at(graph_visited, index, true)
+    over = true
+    updating_graph_mask = Enum.replace_at(updating_graph_mask, index, true)
+    verify_graph(graph_mask, updating_graph_mask, graph_visited, over, others)
+  end
+
+  defp verify_graph(graph_mask, updating_graph_mask, graph_visited, over, []) do
+    {graph_mask, updating_graph_mask, graph_visited, over}
   end
 
   defp for_each_node([node | others], graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes) do
@@ -42,13 +55,17 @@ defmodule BFS_seq do
     {cost, updating_graph_mask}
   end
 
-  def iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, over, iter) when over == false do
+  def iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes) do
+    iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, true, 1)
+  end
+
+  defp iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, over, iter) when over == true do
     {cost, updating_graph_mask} = function1(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes)
-    {graph_mask, graph_visited, over, updating_graph_mask} = function2(graph_mask, updating_graph_mask, graph_visited, over, no_of_nodes)
+    {graph_mask, updating_graph_mask, graph_visited, over} = function2(graph_mask, updating_graph_mask, graph_visited, over, no_of_nodes)
     iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, over, iter + 1)
   end
 
-  def iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, over, iter) when over == true do
+  defp iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes, over, iter) when over == false do
     {iter, cost}
   end
 end
@@ -80,19 +97,10 @@ graph_visited = List.replace_at(graph_visited, source, true)
 edge_list_size = Enum.at(input, no_of_nodes * 2 + 2)
 edges = Enum.slice(input, no_of_nodes * 2 + 3, edge_list_size * 2) |> Enum.chunk_every(2)
 graph_edges = for i <- edges do Enum.at(i, 0) end
-d_cost = List.duplicate(-1, no_of_nodes) |> List.replace_at(0, 0)
+cost = List.duplicate(-1, no_of_nodes) |> List.replace_at(0, 0)
 
 input_read_end = System.monotonic_time()
 IO.puts "Input read time: #{System.convert_time_unit(input_read_end - input_read_start, :native, :millisecond)}ms"
 
-Stream.iterate(0, &(&1 + 1))
-  |> Enum.reduce_while(1, fn _i, acc ->
-    {cost, updating_graph_mask} = BFS_seq.function1(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, d_cost, no_of_nodes)
-    {graph_mask, graph_visited, over, updating_graph_mask} = BFS_seq.function2(graph_mask, updating_graph_mask, graph_visited, false, no_of_nodes)
-    stop = over
-    if !stop do
-      {:halt, acc}
-    else
-      {:cont, acc + 1}
-    end
-  end)
+{i, c} = BFS_seq.iterate(node_list, graph_edges, graph_mask, updating_graph_mask, graph_visited, cost, no_of_nodes)
+IO.puts "#{i}: #{c}"
